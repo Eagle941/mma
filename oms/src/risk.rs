2419adb3de5eaf4ddccb3eb41a13bd8609f154f0
unknown <8973725+Eagle941@users.noreply.github.com>
2026-02-24 00:38:23 +0000

    Updated order logic to avoid trading with myself

--- oms/src/risk.rs
+++ oms/src/risk.rs
@@ -12,6 +12,28 @@ pub enum Outcome {
 // TODO: This file may be moved to a dedicated library
 pub struct RiskManager();
 impl RiskManager {
+    fn get_existing_order(
+        orders_history: &HashMap<String, Order>,
+        side: OrderSide,
+    ) -> Option<(&String, &Order)> {
+        orders_history
+            .iter()
+            .filter(|(_, o)| o.order_status.is_open() && side == o.side)
+            .next()
+    }
+
+    fn get_opposite_order(
+        orders_history: &HashMap<String, Order>,
+        side: OrderSide,
+    ) -> Option<(&String, &Order)> {
+        orders_history
+            .iter()
+            .filter(|(_, o)| {
+                o.order_status.is_open() && side != o.side && OrderSide::NotAvailable != o.side
+            })
+            .next()
+    }
+
     pub fn submit_order(
         orders_history: &HashMap<String, Order>,
         new_order: OrderBuilder,
@@ -31,26 +53,50 @@ impl RiskManager {
         // This is a very simple risk management. Don't have more than two orders
         // running at the same time.
         // NOTE: Assumption is that there is only one active order per side at a time!
-        let Some((_, existing_order)) = orders_history
-            .iter()
-            .filter(|(_, o)| o.order_status.is_open() && new_order.side == o.side)
-            .next()
+        let Some((_, existing_order)) =
+            RiskManager::get_existing_order(orders_history, new_order.side)
         else {
             return Outcome::NewOrder(new_order);
         };
 
+        let new_order_price = f64::from_str(new_order.price.as_str()).unwrap();
         let amended_order = OrderAmendedBuilder {
             symbol: new_order.symbol,
             order_id: existing_order.order_id.clone(),
             qty: new_order.qty,
             price: new_order.price.clone(),
             // TODO: is it more efficient to compare String instead of f64?
-            new_price: f64::from_str(new_order.price.as_str()).unwrap() != existing_order.price,
+            new_price: new_order_price != existing_order.price,
             new_qty: new_order.qty != existing_order.qty,
         };
         if !amended_order.new_price && !amended_order.new_qty {
             return Outcome::Nothing;
         }
-        return Outcome::AmendOrder(amended_order);
+
+        let Some((_, opposite_order)) =
+            RiskManager::get_opposite_order(orders_history, new_order.side)
+        else {
+            return Outcome::AmendOrder(amended_order);
+        };
+
+        if !amended_order.new_price {
+            return Outcome::AmendOrder(amended_order);
+        }
+
+        match new_order.side {
+            OrderSide::Buy => {
+                if new_order_price >= opposite_order.price {
+                    return Outcome::Nothing;
+                }
+                return Outcome::AmendOrder(amended_order);
+            }
+            OrderSide::Sell => {
+                if new_order_price <= opposite_order.price {
+                    return Outcome::Nothing;
+                }
+                return Outcome::AmendOrder(amended_order);
+            }
+            OrderSide::NotAvailable => return Outcome::AmendOrder(amended_order),
+        };
     }
 }
