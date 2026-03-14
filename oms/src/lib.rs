7911074dc7978c5e87801d48a671452303815fb4
unknown <8973725+Eagle941@users.noreply.github.com>
2026-03-14 00:48:46 +0000

    Rustfmt

--- oms/src/lib.rs
+++ oms/src/lib.rs
@@ -1,6 +1,6 @@
 use std::f64;
-use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use crossbeam_channel::Receiver;
