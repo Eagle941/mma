f74a8ddd0bbb85cbce83b2928b942d68f2cbcde7
unknown <8973725+Eagle941@users.noreply.github.com>
2026-03-15 20:21:29 +0000

    Removed hard price floor in risk manager

--- oms/src/risk.rs
+++ oms/src/risk.rs
@@ -9,12 +9,9 @@ pub enum Outcome {
     DoNothing,
 }
 
-pub const MAKER_FEE: f64 = 0.0676; // %
-
 // NOTE: could be dynamic
-pub const MAX_INVENTORY: f64 = 500.0; // Quantity
-pub const MIN_INVENTORY: f64 = -500.0; // Quantity
-pub const NEUTRAL_INVENTORY_THOLD: f64 = 0.01; // Ratio (1%)
+const MAX_INVENTORY: f64 = 500.0; // Quantity
+const MIN_INVENTORY: f64 = -500.0; // Quantity
 
 // TODO: This file may be moved to a dedicated library
 pub struct RiskManager();
@@ -25,23 +22,11 @@ impl RiskManager {
             .find(|(_, o)| o.order_status.is_open() && side == o.side)
     }
 
-    // fn get_opposite_order(
-    //     orders: &HashMap<String, Order>,
-    //     side: OrderSide,
-    // ) -> Option<(&String, &Order)> {
-    //     orders
-    //         .iter()
-    //         .filter(|(_, o)| {
-    //             o.order_status.is_open() && side != o.side &&
-    // OrderSide::NotAvailable != o.side         })
-    //         .next()
-    // }
-
     pub fn submit_order(
         orders: &Slab<Order>,
         new_order: OrderBuilder,
         inventory: f64,
-        average_entry_price: f64,
+        _average_entry_price: f64,
     ) -> Outcome {
         if inventory >= MAX_INVENTORY && new_order.side == OrderSide::Buy {
             return Outcome::DoNothing;
@@ -52,21 +37,6 @@ impl RiskManager {
         }
 
         let new_order_price = f64::from_str(new_order.price.as_str()).unwrap();
-        // When inventory is around 0+/-2%, ignore any limiting.
-        if inventory > MAX_INVENTORY * NEUTRAL_INVENTORY_THOLD && new_order.side == OrderSide::Sell
-        {
-            let minimum_sell_price = average_entry_price * (1.0 + (MAKER_FEE / 100.0) * 2.0);
-            if new_order_price <= minimum_sell_price {
-                return Outcome::DoNothing;
-            }
-        } else if inventory < MIN_INVENTORY * NEUTRAL_INVENTORY_THOLD
-            && new_order.side == OrderSide::Buy
-        {
-            let maximum_buy_price = average_entry_price * (1.0 - (MAKER_FEE / 100.0) * 2.0);
-            if new_order_price >= maximum_buy_price {
-                return Outcome::DoNothing;
-            }
-        }
 
         // This is a very simple risk management. Don't have more than two orders
         // running at the same time.
